fe170cd3ce00cbaa60adf69853c54eedeadca621
Asif-newaz <asif.newaz0093@gmail.com>
2020-06-30 03:20:52 +0600

    Solved challenge 1 using if else condition

--- coding-challenge/script.js
+++ coding-challenge/script.js
@@ -1,4 +1,14 @@
 //Challenge 1
+/*
+Mark and John are trying to compare their BMI (Body Mass Index), which is calculated using the formula: BMI = mass / height^2 = mass / (height * height). (mass in kg and height in meter).
+1. Store Mark's and John's mass and height in variables
+2. Calculate both their BMIs
+3. Create a boolean variable containing information about whether Mark has a higher BMI than John.
+4. Print a string to the console containing the variable from step 3. (Something like "Is Mark's BMI higher than John's? true"). 
+GOOD LUCK 😀
+*/
+
+// Solved normal way
 var massMark = 78; //kg
 var heightMark = 1.69; //meters
 
@@ -12,3 +22,36 @@ console.log(BMIMark, BMIJohn);
 
 var markHigherBMI = BMIMark > BMIJohn;
 console.log('Is Mark\'s BMI higher than John\'s? ' + markHigherBMI);
+
+//Solved challenge 1 using if else condition
+var massMark = 78; //kg
+var heightMark = 1.69; //meters
+
+var massJohn = 92;
+var heightJohn = 1.95;
+
+
+var BMIMark = massMark / (heightMark * heightMark);
+var BMIJohn = massJohn / (heightJohn * heightJohn);
+
+if (BMIMark > BMIJohn) {
+    console.log('Mark\'s BMI is higher than John\'s');
+} else {
+    console.log('John\'s BMI is higher than Mark\'s');
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
