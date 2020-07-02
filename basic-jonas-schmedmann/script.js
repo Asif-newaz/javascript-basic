f1b97ba10f498ecfef54d388dad2cd1dcb85caba
Asif-newaz <asif.newaz0093@gmail.com>
2020-07-03 03:38:06 +0600

    worked with the switch statement

--- basic-jonas-schmedmann/script.js
+++ basic-jonas-schmedmann/script.js
@@ -170,12 +170,54 @@ console.log(firstName + ' ' + drink);
 
 
 /*Switch statement*/
+var job = 'teacher';
+
+switch(job) {
+    case 'teacher':
+    case 'instructor':
+        console.log(firstName + ' teaches kids how to code.');
+        break;    
+    case 'driver':
+        console.log(firstName + ' drives an Uber in Dhaka.');
+        break;
+    case 'designer':
+        console.log(firstName + ' designs a beautiful websites.');
+        break;
+    default:
+        console.log(firstName + ' does something else.');
+}
+
+age = 56;
+switch(false) {
+    case age < 13:
+        console.log(firstName + ' is a boy.');
+        break;
+    case age >= 13 && age < 20:
+        console.log(firstName + ' is a teenager.');
+        break;
+    case age <= 20 && age < 30:
+        console.log(firstName + ' is a young man.');
+        break;
+    default:
+        console.log(firstName + ' is a man.');
+}
 
 
+/*
+var firstName = 'John';
+var age = 20;
 
-
-
-
+if (age < 13) {
+    console.log(firstName + ' is a boy.');
+} else if (age >= 13 && age < 20) {
+    console.log(firstName + ' is a teenager.');
+} else if (age <= 20 && age < 30 ) {
+    console.log(firstName + ' is a young man.');
+}
+else {
+    console.log(firstName + ' is a man.');
+}
+*/
 
 
 
