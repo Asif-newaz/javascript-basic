03a9335a7aaed1b54d8318151bcbc463b4d0e4ab
Asif-newaz <asif.newaz0093@gmail.com>
2020-07-11 03:21:30 +0600

    started working on coding challenge 3

--- coding-challenge/script.js
+++ coding-challenge/script.js
@@ -108,6 +108,7 @@ if (johnScore > mikeScore && johnScore > maryScore) {
 }
 */
 
+/*
 var saymaFirstGame = 129;
 var saymaSecondGame = 129;
 var saymaThirdGame = 105;
@@ -139,6 +140,28 @@ if (saymaScore > tithiScore && saymaScore > urbanaScore) {
 } else {
     console.log('The game is drawn');
 }
+*/
+
+
+
+/*****************************
+* CODING CHALLENGE 3
+*/
+
+/*
+John and his family went on a holiday and went to 3 different restaurants. The bills were $124, $48 and $268.
+To tip the waiter a fair amount, John created a simple tip calculator (as a function). He likes to tip 20% of the bill when the bill is less than $50, 15% when the bill is between $50 and $200, and 10% if the bill is more than $200.
+In the end, John would like to have 2 arrays:
+1) Containing all three tips (one for each bill)
+2) Containing all three final paid amounts (bill + tip).
+(NOTE: To calculate 20% of a value, simply multiply it with 20/100 = 0.2)
+GOOD LUCK 😀
+*/
+
+
+
+
+
 
 
 
