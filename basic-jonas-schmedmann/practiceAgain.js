4ca56ae896542ccf345c122d1fc5a97636b056c8
Asif-newaz <asif.newaz0093@gmail.com>
2021-05-23 00:56:59 +0600

    learn js type coercion

--- basic-jonas-schmedmann/practiceAgain.js
+++ basic-jonas-schmedmann/practiceAgain.js
@@ -1,1 +1,16 @@
 // Want to practice again
+
+var firstName = 'John';
+var lastName = 'smith';
+var age = 28;
+
+console.log('My name is ' + firstName + ' ' + lastName +'.' + `I'm ` + age + 'years old.');
+
+/* Variable mutation and type coercion */
+
+var job, isMarried;
+
+job = 'teacher'
+isMarried = false;
+
+console.log(job, isMarried)
