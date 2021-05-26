588eb8fcb18796caa61344940a6645bc3b1d364d
Asif-newaz <asif.newaz0093@gmail.com>
2021-05-27 01:38:29 +0600

    operator precedence

--- basic-jonas-schmedmann/practiceAgain.js
+++ basic-jonas-schmedmann/practiceAgain.js
@@ -1,16 +1,29 @@
 // Want to practice again
 
-var firstName = 'John';
-var lastName = 'smith';
-var age = 28;
+// var firstName = 'John';
+// var lastName = 'smith';
+// var age = 28;
 
-console.log('My name is ' + firstName + ' ' + lastName +'.' + `I'm ` + age + 'years old.');
+// console.log('My name is ' + firstName + ' ' + lastName +'.' + `I'm ` + age + 'years old.');
 
 /* Variable mutation and type coercion */
 
-var job, isMarried;
+// var job, isMarried;
 
-job = 'teacher'
-isMarried = false;
+// job = 'teacher'
+// isMarried = false;
 
-console.log(job, isMarried)
+// console.log(job, isMarried)
+
+// Operator precedence
+var now = 2021;
+var john = 2008;
+var fullAge = 20;
+
+var isFullAge = now - john >= fullAge;
+console.log(isFullAge);
+
+var ageJohn = now - john;
+var markAge = 30;
+var average = (ageJohn + markAge) / 2;
+console.log(average);
