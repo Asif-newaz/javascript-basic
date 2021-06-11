f85e202886e419f6cbe3507a845b71075e932f0c
Asif-newaz <asif.newaz0093@gmail.com>
2021-06-12 00:19:01 +0600

    work with js type coercion

--- basic-jonas-schmedmann/practiceAgain.js
+++ basic-jonas-schmedmann/practiceAgain.js
@@ -74,41 +74,62 @@
 //   console.log(firstName + " is a man");
 // }
 
-var firstName = "John";
+// var firstName = "John";
 // var age = 18;
 
 // var drink = age >= 18 ? firstName + " drinks beer" : firstName + " drinks juice";
 // console.log(drink);
 
-var job = "constructor";
+// var job = "constructor";
 
-switch (job) {
-  case "teacher":
-  case "constructor":
-    console.log(firstName + " teaches kids how to code.");
-    break;
-  case "driver":
-    console.log(firstName + " drives an uber in Dhaka.");
-    break;
-  case "designer":
-    console.log(firstName + " designs beautiful websites.");
-    break;
-  default:
-    console.log(firstName + " does something else.");
+// switch (job) {
+//   case "teacher":
+//   case "constructor":
+//     console.log(firstName + " teaches kids how to code.");
+//     break;
+//   case "driver":
+//     console.log(firstName + " drives an uber in Dhaka.");
+//     break;
+//   case "designer":
+//     console.log(firstName + " designs beautiful websites.");
+//     break;
+//   default:
+//     console.log(firstName + " does something else.");
+// }
+
+// var age = 24;
+
+// switch (true) {
+//   case age < 13:
+//     console.log(firstName + " is a boy");
+//     break;
+//   case age >= 13 && age < 20:
+//     console.log(firstName + " is a teenager");
+//     break;
+//   case age >= 20 && age <= 30:
+//     console.log(firstName + " is a young man");
+//     break;
+//   default:
+//     console.log(firstName + " is a man");
+// }
+
+// Falsy Values: undefined, 0, ' ', null, NaN
+// Truthy Values: Not falsy values
+
+
+var height;
+height = NaN;
+
+if (height) {
+  console.log("variable is defined");
+} else {
+  console.log("variable has not been defined");
 }
 
-var age = 20;
+var x = '0';
 
-switch(false) {
-  case (age < 13):
-    console.log(firstName + " is a boy");
-    break;
-  case (age >= 13 && age < 20):
-    console.log(firstName + " is a teenager");
-    break;
-  case (age >= 20 && age <= 30):
-    console.log(firstName + " is a young man");
-    break;
-  default:
-    console.log(firstName + " is a man");
+if (x == 0 ) {
+  console.log('The == operator does type coercion!')
+} else {
+  console.log('The === operator does not type coercion')
 }
