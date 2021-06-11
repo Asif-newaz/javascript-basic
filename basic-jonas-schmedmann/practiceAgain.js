2e9547b8a5c827563147bb4e7a280dea5260e731
Asif-newaz <asif.newaz0093@gmail.com>
2021-06-11 19:07:50 +0600

    work with switch statement

--- basic-jonas-schmedmann/practiceAgain.js
+++ basic-jonas-schmedmann/practiceAgain.js
@@ -61,15 +61,54 @@
 //   console.log(firsName + " will marry soon!!!");
 // }
 
+// var firstName = "John";
+// var age = 20;
+
+// if (age < 13) {
+//   console.log(firstName + " is a boy");
+// } else if (age >= 13 && age < 20) {
+//   console.log(firstName + " is a teenager");
+// } else if (age >= 20 && age <= 30) {
+//   console.log(firstName + " is a young man");
+// } else {
+//   console.log(firstName + " is a man");
+// }
+
 var firstName = "John";
+// var age = 18;
+
+// var drink = age >= 18 ? firstName + " drinks beer" : firstName + " drinks juice";
+// console.log(drink);
+
+var job = "constructor";
+
+switch (job) {
+  case "teacher":
+  case "constructor":
+    console.log(firstName + " teaches kids how to code.");
+    break;
+  case "driver":
+    console.log(firstName + " drives an uber in Dhaka.");
+    break;
+  case "designer":
+    console.log(firstName + " designs beautiful websites.");
+    break;
+  default:
+    console.log(firstName + " does something else.");
+}
+
 var age = 20;
 
-if (age < 13) {
-  console.log(firstName + " is a boy");
-} else if (age >= 13 && age < 20) {
-  console.log(firstName + " is a teenager");
-} else if (age >= 20 && age <= 30) {
-  console.log(firstName + " is a young man");
-} else {
-  console.log(firstName + " is a man");
+switch(false) {
+  case (age < 13):
+    console.log(firstName + " is a boy");
+    break;
+  case (age >= 13 && age < 20):
+    console.log(firstName + " is a teenager");
+    break;
+  case (age >= 20 && age <= 30):
+    console.log(firstName + " is a young man");
+    break;
+  default:
+    console.log(firstName + " is a man");
 }
