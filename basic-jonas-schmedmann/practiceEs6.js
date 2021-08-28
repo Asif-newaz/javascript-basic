974a2d4a47b952f11bc7cfa58c3e29ddf3bcfa3b
Asif-newaz <asif.newaz0093@gmail.com>
2021-08-28 14:40:51 +0600

    started working with new course

---
+++ basic-jonas-schmedmann/practiceEs6.js
@@ -0,0 +1,44 @@
+// ES6 practice
+// const now = 2037;
+// const ageDavid = now - 1994;
+// const ageSarah = now - 2018;
+
+// console.log(ageDavid, ageSarah);
+
+// let x = 10 + 5;
+// x += 5;
+// x *= 4;
+// x++;
+// x--;
+// x--;
+// console.log(x);
+
+// console.log(ageDavid < ageSarah);
+// console.log(ageSarah >= 18);
+
+// const fullAddress = {
+//   name: "Asif",
+//   profession: "Job",
+//   address: {
+//     village: "Sonmania",
+//     district: "Gazipur",
+//   },
+// };
+
+// console.log(fullAddress.address?.village);
+// console.log(fullAddress.credentials?.district);
+// console.log(fullAddress.occupation);
+
+const massMark = 78;
+const heightMark = 1.69;
+const massJohn = 92;
+const heightJohn = 1.95;
+
+const bmiMark = massMark / heightMark ** 2;
+const bmiJohn = massJohn / heightJohn ** 2;
+
+console.log(bmiMark);
+console.log(bmiJohn);
+
+const markHigherBmi = bmiMark > bmiJohn;
+console.log("Mark has a higher BMI than John" + " " + markHigherBmi);
