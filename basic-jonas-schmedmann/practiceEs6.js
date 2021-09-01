b0fb15a42a377b7ff420b00502053896f06a0014
Asif-newaz <asif.newaz0093@gmail.com>
2021-09-01 23:38:47 +0600

    finish challenge 2

--- basic-jonas-schmedmann/practiceEs6.js
+++ basic-jonas-schmedmann/practiceEs6.js
@@ -45,9 +45,26 @@
 // const markHigherBmi = bmiMark > bmiJohn;
 // console.log("Mark has a higher BMI than John" + " " + markHigherBmi);
 
-const firstName = "David";
-const job = "Programer";
-const age = 2037 - 1991;
+// const firstName = "David";
+// const job = "Programer";
+// const age = 2037 - 1991;
 
-console.log("I'm" + " " + firstName + ", a " + age + " years old " + job);
-console.log(`I'm ${firstName}, a ${age} years old ${job}`);
+// console.log("I'm" + " " + firstName + ", a " + age + " years old " + job);
+// console.log(`I'm ${firstName}, a ${age} years old ${job}`);
+
+// Coding challenge 2
+const massMark = 78;
+const heightMark = 1.69;
+const massJohn = 92;
+const heightJohn = 1.95;
+
+const bmiMark = massMark / heightMark ** 2;
+const bmiJohn = massJohn / heightJohn ** 2;
+
+const markHigherBmi = bmiMark > bmiJohn;
+
+if (bmiMark > bmiJohn) {
+  console.log(`Mark's BMI ${bmiMark} is higher than John's BMI ${bmiJohn}`);
+} else {
+  console.log(`John's BMI ${bmiJohn} is higher than Mark's BMI ${bmiMark}`);
+}
